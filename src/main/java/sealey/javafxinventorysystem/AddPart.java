1113cf8004003467edc88393f6b3b9b8fd79380d
Max Sealey <maxsealey@users.noreply.github.com>
2023-01-07 18:54:12 -0800

    Implemented scene navigation

--- src/main/java/sealey/javafxinventorysystem/AddPart.java
+++ src/main/java/sealey/javafxinventorysystem/AddPart.java
@@ -1,17 +1,26 @@
 package sealey.javafxinventorysystem;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class AddPart implements Initializable {
+    Stage stage;
+    Parent scene;
     @FXML
     private ToggleGroup addpartgroup;
 
@@ -50,6 +59,24 @@ public class AddPart implements Initializable {
 
     @FXML
     private Button saveButton;
+
+    @FXML
+    void onActionCancel(ActionEvent event) throws IOException {
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainWindow.fxml")));
+        stage.setScene(new Scene(scene));
+        stage.show();
+    }
+
+    @FXML
+    void onActionCompanyLabel(ActionEvent event) {
+        System.out.println("Change machine id label to company name");
+    }
+
+    @FXML
+    void onActionSave(ActionEvent event) {
+        System.out.println("Save button pressed");
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
--- src/main/java/sealey/javafxinventorysystem/ModifyProduct.java
+++ src/main/java/sealey/javafxinventorysystem/ModifyProduct.java
@@ -1,16 +1,25 @@
 package sealey.javafxinventorysystem;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class ModifyProduct implements Initializable {
+    Stage stage;
+    Parent scene;
 
     @FXML
     private Button addButton;
@@ -75,9 +84,16 @@ public class ModifyProduct implements Initializable {
     @FXML
     private TableColumn<?, ?> table2PriceCol;
 
+    @FXML
+    void onActionCancel(ActionEvent event) throws IOException {
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainWindow.fxml")));
+        stage.setScene(new Scene(scene));
+        stage.show();
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println("initialized");
     }
-
 }
