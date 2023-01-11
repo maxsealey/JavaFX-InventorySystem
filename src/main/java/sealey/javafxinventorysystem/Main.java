856efa1efbac86d986127abd357fcee354aec82a
Max Sealey <maxsealey@users.noreply.github.com>
2023-01-10 22:38:37 -0800

    Added additional test data, including associated parts to products

--- src/main/java/sealey/javafxinventorysystem/Main.java
+++ src/main/java/sealey/javafxinventorysystem/Main.java
@@ -40,8 +40,10 @@ public class Main extends Application {
 
         OutSourced part1 = new OutSourced(1, "wheel", 34.99, 16, 5, 30);
         OutSourced part2 = new OutSourced(2, "brake", 29.99, 18, 10, 25);
-        InHouse part3 = new InHouse(3, "handlebars", 39.99, 5, 5, 15);
-        InHouse part4 = new InHouse(4, "chain", 14.99, 9, 7, 20);
+        OutSourced part3 = new OutSourced(3, "headlight", 49.99, 18, 10, 25);
+        InHouse part4 = new InHouse(4, "handlebars", 19.99, 5, 5, 15);
+        InHouse part5 = new InHouse(5, "chain", 64.99, 9, 7, 20);
+        InHouse part6 = new InHouse(6, "steering wheel", 54.99, 9, 7, 20);
 
         Product prod1 = new Product(1, "Bicycle", 149.99, 12, 8, 20);
         Product prod2 = new Product(2, "Unicycle", 134.99, 6, 3, 10);
@@ -49,10 +51,25 @@ public class Main extends Application {
 
         part1.setCompanyName("Headgum");
         part2.setCompanyName("OMSB");
-        part3.setMachineId(87);
-        part4.setMachineId(32);
+        part2.setCompanyName("Zona Gale");
+        part4.setMachineId(87);
+        part5.setMachineId(32);
+        part6.setMachineId(99);
 
-        ObservableList<Part> parts = FXCollections.observableArrayList(part1,part2,part3,part4);
+        prod1.addAssociatedPart(part4);
+        prod1.addAssociatedPart(part3);
+
+        prod2.addAssociatedPart(part3);
+        prod2.addAssociatedPart(part2);
+        prod2.addAssociatedPart(part5);
+
+        prod3.addAssociatedPart(part2);
+        prod3.addAssociatedPart(part1);
+        prod3.addAssociatedPart(part3);
+        prod3.addAssociatedPart(part5);
+        prod3.addAssociatedPart(part6);
+
+        ObservableList<Part> parts = FXCollections.observableArrayList(part1,part2,part3,part4,part5,part6);
         Inventory.setAllParts(parts);
 
         ObservableList<Product> products = FXCollections.observableArrayList(prod1,prod2,prod3);
